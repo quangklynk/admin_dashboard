de23c94f8e0672c4f73274e5842ef63a859d5a31
Võ Ngọc Tỷ <apple@Apples-MacBook-Pro.local>
2021-08-16 22:47:40 +0700

    dasd

--- Desktop/DA_THUCTAP/BE/src/app/Http/Controllers/EmployeeController/ProductController.php
+++ Desktop/DA_THUCTAP/BE/src/app/Http/Controllers/EmployeeController/ProductController.php
@@ -142,26 +142,36 @@ class ProductController extends Controller
     }
 
     public function updateProductImage(Request $request){
-
-        if ($request->imgAvatar) {
-            $file = $request->file('imgAvatar')->getClientOriginalName();
-            // $filename = pathinfo($file, PATHINFO_FILENAME); đuôi file
-            $filenameA = date('Y_m_d_H_i_s');
-            $extension = pathinfo($file, PATHINFO_EXTENSION);
-            $filename = "SP_A"  .  $filenameA . "." . $extension;
-        }
+        //request: idPro, 
         DB::beginTransaction();
+
         try {
+
+            if ($request->imgAvatar) {
+                $file = $request->file('imgAvatar')->getClientOriginalName();
+                // $filename = pathinfo($file, PATHINFO_FILENAME); đuôi file
+                $filenameA = date('Y_m_d_H_i_s');
+                $extension = pathinfo($file, PATHINFO_EXTENSION);
+                $filename = "SP_A"  .  $filenameA . "." . $extension;
+
+                Product::updateOrCreate(
+                    ['id' => $request->idPro],
+                    [
+                        'avatar' => $filename,
+                     ],
+                );
+
+            }
         
             if ($request->img1) {
                 $file = $request->file('img1')->getClientOriginalName();
                 $filename = date('Y_m_d_H_i_s');
                 $extension = pathinfo($file, PATHINFO_EXTENSION);
                 $filename = "SP_1" . $request->idProduct . $filename . "." . $extension;
-                DB::table('list__images')->insert(
+                List_Image::updateOrCreate(
+                            ['id' => $request->idImg1],
                             [
                                 'image' => $filename,
-                                'idProduct' => $pro->id
                             ],
                 );
                 $request->file('img1')->move(public_path("/image/product"), $filename);
@@ -172,11 +182,11 @@ class ProductController extends Controller
                 $filename = date('Y_m_d_H_i_s');
                 $extension = pathinfo($file, PATHINFO_EXTENSION);
                 $filename = "SP_2" . $request->idProduct . $filename . "." . $extension;
-                DB::table('list__images')->insert(
-                            [
-                                'image' => $filename,
-                                'idProduct' => $pro->id
-                            ],
+                List_Image::updateOrCreate(
+                    ['id' => $request->idImg2],
+                    [
+                        'image' => $filename,
+                    ],
                 );
                 $request->file('img2')->move(public_path("/image/product"), $filename);
             }
@@ -185,11 +195,11 @@ class ProductController extends Controller
                 $filename = date('Y_m_d_H_i_s');
                 $extension = pathinfo($file, PATHINFO_EXTENSION);
                 $filename = "SP_3" . $request->idProduct . $filename . "." . $extension;
-                DB::table('list__images')->insert(
-                            [
-                                'image' => $filename,
-                                'idProduct' => $pro->id
-                            ],
+                List_Image::updateOrCreate(
+                    ['id' => $request->idImg3],
+                    [
+                        'image' => $filename,
+                    ],
                 );
                 $request->file('img3')->move(public_path("/image/product"), $filename);
             }
@@ -198,11 +208,11 @@ class ProductController extends Controller
                 $filename = date('Y_m_d_H_i_s');
                 $extension = pathinfo($file, PATHINFO_EXTENSION);
                 $filename = "SP_4" . $request->idProduct . $filename . "." . $extension;
-                DB::table('list__images')->insert(
-                            [
-                                'image' => $filename,
-                                'idProduct' => $pro->id
-                            ],
+                List_Image::updateOrCreate(
+                    ['id' => $request->idImg4],
+                    [
+                        'image' => $filename,
+                    ],
                 );
                 $request->file('img4')->move(public_path("/image/product"), $filename);
             }

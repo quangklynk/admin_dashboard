27f4801b5244c967a474f46c69049279ac6dacc0
Quang Trương <quangklynk@gmail.com>
2021-08-20 15:57:26 +0700

    project mana

---
+++ Desktop/DA_THUCTAP/BE/src/app/Http/Controllers/EmployeeController/EnterStickerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\EmployeeController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EnterStickerController extends Controller
+{
+    //
+}
